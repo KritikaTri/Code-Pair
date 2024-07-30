8da28625cdb57ad5bae6eb2ac9922be926e82c79
Demo-Git <kritika.tripathi.mec21@itbhu.ac.in>
2024-07-31 05:17:20 +0530

    first commit

---
+++ src/util/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer>
+			<h4>
+				Built With 💛 by &nbsp;<a href='https://github.com/KritikaTri'>Kritika Tripathi</a>
+			</h4>
+		</footer>
+	);
+};
+
+export default Footer;
